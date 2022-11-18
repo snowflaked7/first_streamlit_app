839361fbd6899a23a00c4f0db0d6aba038885482
snowflaked7 <118425933+snowflaked7@users.noreply.github.com>
2022-11-18 15:27:18 +0530

    Update streamlit_app.py
    added fruity vice header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
   
 streamlit.title('🍞 My Streamlit App')
 streamlit.header('🥑 November 17th')
@@ -24,3 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
