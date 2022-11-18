502df4cb031c3818b851fc15d1e0fa82dedafc95
snowflaked7 <118425933+snowflaked7@users.noreply.github.com>
2022-11-18 15:26:04 +0530

    Update streamlit_app.py
    Added frutiy vice api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 
 import streamlit 
 import pandas
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
   
 streamlit.title('🍞 My Streamlit App')
 streamlit.header('🥑 November 17th')
